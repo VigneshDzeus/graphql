d43f70d73537823179b652abac79048ce18e287e
Vignesh D <samsekaran@gmail.com>
2023-01-04 13:31:29 +0530

    initial commit

---
+++ schema/schema.js
@@ -0,0 +1,43 @@
+const graphql = require("graphql");
+const _ = require("lodash");
+
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+//mock data
+var books = [
+  { name: "book1", id: "1", genre: "Fantasy" },
+  { name: "book2", id: "2", genre: "Sci-Fi" },
+  { name: "book3", id: "3", genre: "Horror" },
+];
+
+const BookType = new GraphQLObjectType({
+  name: "Book",
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    hello: {
+      type: GraphQLString,
+      resolve: () => "hello world", //Resolver
+    },
+    book: {
+      type: BookType,
+      args: { id: { type: GraphQLString } },
+      resolve: (parent, args) => {
+        // code to get data from DB
+        console.log("kjb");
+        return_.find(books, { id: args.id });
+      },
+    },
+  },
+});
+
+module.export = new GraphQLSchema({
+  query: RootQuery,
+});
